9eb1aa26493845aeb936d0d7f860e1282c87ca3d
SergBily <sergbily.dev@gmail.com>
2023-07-16 20:46:16 +0300

    fix: create ship coordinate

--- app/components/game/domain/create-positions-ship.ts
+++ app/components/game/domain/create-positions-ship.ts
@@ -11,20 +11,38 @@ const fillPositions = (ship: Ships, type: string, axis: string): number[] => {
   return Array.from({ length: ship.length }, (_v) => ship.position[axis as Axis]);
 };
 
-export const createPositionsShip = (ships: Ships[]): CoordinateShips => {
-  const filledPositionsShips: CoordinateShips = {
-    closePosition: [],
-    openPosition: [],
+export const createPositionsShip = (ships: Ships[]): Record <string, CoordinateShips> => {
+  const filledPositionsShips: Record <string, CoordinateShips> = {
+    0: {
+      openPosition: [],
+      closePosition: [],
+    },
   };
-  for (const ship of ships) {
+  // for (const ship of ships) {
+  //   if (ship.direction) {
+  //     const y = fillPositions(ship, 'add', 'y');
+  //     const x = fillPositions(ship, '', 'x');
+  //     filledPositionsShips.closePosition.push({ x, y });
+  //   } else {
+  //     const y = fillPositions(ship, '', 'y');
+  //     const x = fillPositions(ship, 'add', 'x');
+  //     filledPositionsShips.closePosition.push({ x, y });
+  //   }
+  // }
+
+  for (const [index, ship] of ships.entries()) {
     if (ship.direction) {
       const y = fillPositions(ship, 'add', 'y');
       const x = fillPositions(ship, '', 'x');
-      filledPositionsShips.closePosition.push({ x, y });
+      const bb: CoordinateShips | undefined = filledPositionsShips[index];
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      bb['closePosition'].push({ x, y });
     } else {
       const y = fillPositions(ship, '', 'y');
       const x = fillPositions(ship, 'add', 'x');
-      filledPositionsShips.closePosition.push({ x, y });
+      const bb: CoordinateShips | undefined = filledPositionsShips[index];
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      bb['closePosition'].push({ x, y });
     }
   }
   return filledPositionsShips;
